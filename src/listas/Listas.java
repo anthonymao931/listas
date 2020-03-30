b1475c72b4503d3b3a583fb2547653f28786b16c
anthonymao931 <u1803646@unimilitar.edu.co>
2020-03-30 10:35:09 -0500

    final

--- src/listas/Listas.java
+++ src/listas/Listas.java
@@ -23,15 +23,31 @@ public class Listas {
 ArrayList<familia> listapersona= new ArrayList<familia>();
 System.out.println("tamaño de la lista: "+listapersona.size());
 
-familia p1 = new familia(35,"Lucia Mama", 35.4);
-familia p2 = new familia(18,"Martin Hijo", 35.6);
+familia p1 = new familia(35,"Lucia -Mama", 35.4);
+familia p2 = new familia(18,"Mario -Hijo", 36.6);
+familia p3 = new familia(18,"Jesus -Padre", 37.6);
+familia p4 = new familia(18,"Ana -Hija", 36.4);
+familia p5 = new familia(18,"Jaime -Abuelo", 35.8);
+familia p6 = new familia(18,"Maria -Abuela", 36.9);
 listapersona.add(p1);
 listapersona.add(p2);
+listapersona.add(p3);
+listapersona.add(p4);
+listapersona.add(p5);
+listapersona.add(p6);
 p1.mostrardatos();
 p2.mostrardatos();
+p3.mostrardatos();
+p4.mostrardatos();
+p5.mostrardatos();
+p6.mostrardatos();
 System.out.println("la temperatura de todos los mienbros de la familia es ");
 p1.datostemperatura();
 p2.datostemperatura();
+p3.datostemperatura();
+p4.datostemperatura();
+p5.datostemperatura();
+p6.datostemperatura();
 
 
 System.out.println("tamaño de la lista despues de primer elemento: "+listapersona.size());

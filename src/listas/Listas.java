628c69712f502b7013daad62eaaf11d985613455
Andres <u1803646@unimitar.edu.co>
2020-03-30 15:04:16 -0500

    ejercicio 2.0 master

--- src/listas/Listas.java
+++ src/listas/Listas.java
@@ -6,6 +6,7 @@
 package listas;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 /**
@@ -19,41 +20,21 @@ public class Listas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-     
+Scanner entradateclado = new Scanner(System.in);
 ArrayList<familia> listapersona= new ArrayList<familia>();
-System.out.println("tamaño de la lista: "+listapersona.size());//DIEGO ES GEI
-
-familia p1 = new familia(35,"Lucia -Mama", 35.4);
-familia p2 = new familia(18,"Mario -Hijo", 36.6);
-familia p3 = new familia(18,"Jesus -Padre", 37.6);
-familia p4 = new familia(18,"Ana -Hija", 36.4);
-familia p5 = new familia(18,"Jaime -Abuelo", 35.8);
-familia p6 = new familia(18,"Maria -Abuela", 36.9);
-listapersona.add(p1);
-listapersona.add(p2);
-listapersona.add(p3);
-listapersona.add(p4);
-listapersona.add(p5);
-listapersona.add(p6);
-p1.mostrardatos();
-p2.mostrardatos();
-p3.mostrardatos();
-p4.mostrardatos();
-p5.mostrardatos();
-p6.mostrardatos();
-
-System.out.println("***************************************************** ");
-System.out.println("la temperatura de todos los mienbros de la familia es ");
-System.out.println("***************************************************** ");
-
-p1.datostemperatura();
-p2.datostemperatura();
-p3.datostemperatura();
-p4.datostemperatura();
-p5.datostemperatura();
-p6.datostemperatura();
-
-
-System.out.println("tamaño de la lista despues de primer elemento: "+listapersona.size());
+System.out.println("tamaño de la lista: "+listapersona.size());
+int c;
+ System.out.println("diguite la cantidad de personas");
+ c =entradateclado.nextInt();
+ for(int i=0;i<c;i++){
+     listapersona.add(new familia());
+  System.out.println("Persona creada correctamente.");
+  System.out.println("tamaño de la lista despues de primer elemento: "+listapersona.size());
+ }
+ for(int h=0;h<listapersona.size();h++){
+ System.out.println((h+1)+"."+listapersona.get(h).nombre+"su temperatura es de: "+listapersona.get(h).temperatura);
+     }
+      
     }
-}
+
+    }
--- src/listas/familia.java
+++ src/listas/familia.java
@@ -5,6 +5,9 @@
  */
 package listas;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 
 
@@ -15,25 +18,42 @@ package listas;
  */
 public class familia {
    
-    private String nombre;
-    private int edad;
-    private double temperatura;
-    
-  
-    
-    
-    public familia( int edad,String nombre, double temperatura){
-        this.nombre=nombre;
-        this.edad=edad;
-       this.temperatura=temperatura;
+     Scanner entradateclado = new Scanner(System.in);
+    String nombre;
+    int edad;
+    String parentesco;
+    Float temperatura;
+    ArrayList<familia> listatemperatura= new ArrayList<familia>();
+    familia(){
+        
+        this.nombre = ingresonombre();
+        this.edad = ingresoedad();
+        this.parentesco = ingresoparentesco();
+        this.temperatura = ingresotemperatura();
     }
     
+    Float ingresotemperatura(){
+       System.out.print("Ingrese la temperatura : ");
+        temperatura = entradateclado.nextFloat();
+        return temperatura;
+    }
+        String ingresoparentesco(){
+        
+        System.out.print("Ingrese el parentesco de la persona: ");
+        parentesco = entradateclado.nextLine();  
+        return parentesco;
+    }
+    int ingresoedad(){
+        
+        System.out.print("Ingrese la edad de la persona: ");
+        edad = Integer.parseInt(entradateclado.nextLine());  
+        return edad;
+    }
+    String ingresonombre(){       
+        System.out.print("Ingrese el nombre de la persona: ");
+        nombre = entradateclado.nextLine();  
+        return nombre;
+    }
     
-     public void mostrardatos(){
-      System.out.println("el nombre y parentesco es: "+ nombre + " la edad es: "+ edad);
-      
-                            }
-     public void datostemperatura(){
-         System.out.println(" " + nombre + "  su temperatura es de: "+ temperatura);
-     }
 }
+
